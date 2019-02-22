7a5000dedc6ba191248943f1bc9befd93763ec44
James Davidson <Jamesdvdavidson@gmail.com>
2019-02-22 20:17:41 +0000

    Added article preloading

--- codetest.js
+++ codetest.js
@@ -19,131 +19,182 @@ function init() {
 }
 
 
-function getArticle(filename) {
+function getArticle(filename, showNow=true) {
+
+    if (ArticleExists(filename)){
+        if (showNow) {
+            showArticle(filename);
+        }
+        return;
+    }
+
+    if (filename === undefined){
+        alert("No file name given");
+        return false;
+    }
+
+    var URL = "http://3.8.136.10/bbccodetest/article/" + filename;
+
+    var httpreq = new XMLHttpRequest();
+
+    httpreq.onreadystatechange = function () {
+        if (this.readyState == 4) {
+
+            var jsonArticle = JSON.parse(this.responseText);
+
+            for (var i = 0; i < arrArticles.length; i++) {
+                if (arrArticles[i].filename == filename) {
+
+                    arrArticles[i].json = jsonArticle;
+                    if (showNow){
+                        showArticle(filename);
+                    }
+                }
+
+            }
+
+            //console.log(arrReadArticles);
+
+        }
+    };
+    httpreq.open("GET", URL, true);
+    //httpreq.setRequestHeader("Content-type", "application/json");
+    httpreq.send();
+    return;
+}
+
+function showArticle(filename) {
+    var jsonArticle;
+
     var divContent = document.getElementById("articleContent");
     divContent.innerHTML = "";
     if (filename === undefined){
         alert("No file name given");
         return false;
     }
-    var URL = "http://3.8.136.10/bbccodetest/article/" + filename;
 
-    var httpreq = new XMLHttpRequest();
-
-    httpreq.onreadystatechange = function () {
-        if (this.readyState == 4){
-
-            var jsonArticle = JSON.parse(this.responseText);
-
-            if (arrReadArticles.includes(filename) === false){
-                arrReadArticles.push(filename);
-                for (var i = 0; i < arrArticles.length; i++){
-                    if (arrArticles[i].filename == filename){
-
-                        arrArticles[i].json = jsonArticle;
-                    }
-
-                }
-            }
-            console.log(arrReadArticles);
-
-            var divProgress = document.getElementById("progress");
-
-            if (arrReadArticles.length != 5) {
-                divProgress.innerHTML = "<p>You have read " + arrReadArticles.length + " of today's 5 articles.</p>";
-            } else {
-                divProgress.innerHTML = "<p>You have read all of today's articles, would you like to rank them?</p>" ;
-                var lnkRank = document.createElement("a");
-                lnkRank.href = "#";
-                lnkRank.onclick = function(){
-                    getRanking();
-                };
-                lnkRank.innerText = "Rank articles";
-                divProgress.appendChild(lnkRank);
-            }
-
-
-
-            var divContent = document.getElementById("articleContent");
-
-            //divContent.innerHTML = jsonArticle.title;
-            //divContent.innerHTML = "<h1>" + jsonArticle.title + "</h1>";
-
-            var tagTitle = document.createElement("h2");
-            tagTitle.className = "article";
-            tagTitle.innerHTML = jsonArticle.title;
-            divContent.appendChild(tagTitle);
-
-
-            for (var i = 0; i < jsonArticle.body.length; i++){
-                console.log(jsonArticle.body[i].type);
-                switch (jsonArticle.body[i].type) {
-                    case "heading":
-                        var tagHeading = document.createElement("h3");
-                        tagHeading.className = "article";
-                        tagHeading.innerHTML = jsonArticle.body[i].model.text;
-                        divContent.appendChild(tagHeading);
-                        break;
-                    case "paragraph":
-                        var tagParagraph = document.createElement("p");
-                        tagParagraph.innerHTML = jsonArticle.body[i].model.text;
-                        divContent.appendChild(tagParagraph);
-                        break;
-                    case "image":
-                        var divImage = document.createElement("div");
-                        var tagImage = document.createElement("img");
-                        tagImage.src = jsonArticle.body[i].model.url;
-                        tagImage.alt = jsonArticle.body[i].model.altText;
-                        tagImage.height = jsonArticle.body[i].model.height;
-                        tagImage.width = jsonArticle.body[i].model.width;
-
-                        divImage.appendChild(tagImage);
-
-                        divContent.appendChild(divImage);
-                        break;
-                    case "list":
-                        switch (jsonArticle.body[i].model.type) {
-                            case "unordered":
-                                var tagList = document.createElement("ul");
-                                for (var j = 0; j < jsonArticle.body[i].model.items.length; j++){
-                                    var tagItem = document.createElement("li");
-                                    tagItem.innerHTML = jsonArticle.body[i].model.items[j];
-                                    tagList.appendChild(tagItem);
-                                }
-                                console.log(tagList.innerHTML);
-                                console.log(tagList.outerHTML);
-                                divContent.appendChild(tagList);
-                                break;
-
-                            case "ordered":
-                                var tagList = document.createElement("ol");
-                                for (var j = 0; j < jsonArticle.body[i].model.items.length; j++){
-                                    var tagItem = document.createElement("li");
-                                    tagItem.innerHTML = jsonArticle.body[i].model.items[j];
-                                    tagList.appendChild(tagItem);
-                                }
-                                console.log(tagList.innerHTML);
-                                console.log(tagList.outerHTML);
-                                divContent.appendChild(tagList);
-                                break;
-                        }
-
-                        divContent.appendChild(tagList);
-                        break;
-
-                }
-            }
-
-            generateNextButton(filename);
-
-            return true;
+    for (var i = 0; i < arrArticles.length; i++) {
+        if (arrArticles[i].filename == filename) {
+             jsonArticle = arrArticles[i].json;
         }
     }
 
-    httpreq.open("GET", URL, true);
-    //httpreq.setRequestHeader("Content-type", "application/json");
-    httpreq.send();
-    return;
+    if ( jsonArticle === undefined) {
+        alert("Article not found");
+        return false;
+    }
+
+    if (arrReadArticles.includes(filename) === false){
+        arrReadArticles.push(filename);
+    }
+
+
+    console.log(arrReadArticles);
+
+    var divProgress = document.getElementById("progress");
+
+    if (arrReadArticles.length != 5) {
+        divProgress.innerHTML = "<p>You have read " + arrReadArticles.length + " of today's "+arrArticles.length+" articles.</p>";
+    } else {
+        divProgress.innerHTML = "<p>You have read all of today's articles, would you like to rank them?</p>" ;
+        var lnkRank = document.createElement("a");
+        lnkRank.href = "#";
+        lnkRank.onclick = function(){
+            getRanking();
+        };
+        lnkRank.innerText = "Rank articles";
+        divProgress.appendChild(lnkRank);
+    }
+
+
+
+    var divContent = document.getElementById("articleContent");
+
+    //divContent.innerHTML = jsonArticle.title;
+    //divContent.innerHTML = "<h1>" + jsonArticle.title + "</h1>";
+
+    var tagTitle = document.createElement("h2");
+    tagTitle.className = "article";
+    tagTitle.innerHTML = jsonArticle.title;
+    divContent.appendChild(tagTitle);
+
+
+    for (var i = 0; i < jsonArticle.body.length; i++){
+        console.log(jsonArticle.body[i].type);
+        switch (jsonArticle.body[i].type) {
+            case "heading":
+                var tagHeading = document.createElement("h3");
+                tagHeading.className = "article";
+                tagHeading.innerHTML = jsonArticle.body[i].model.text;
+                divContent.appendChild(tagHeading);
+                break;
+            case "paragraph":
+                var tagParagraph = document.createElement("p");
+                tagParagraph.innerHTML = jsonArticle.body[i].model.text;
+                divContent.appendChild(tagParagraph);
+                break;
+            case "image":
+                var divImage = document.createElement("div");
+                var tagImage = document.createElement("img");
+                tagImage.src = jsonArticle.body[i].model.url;
+                tagImage.alt = jsonArticle.body[i].model.altText;
+                tagImage.height = jsonArticle.body[i].model.height;
+                tagImage.width = jsonArticle.body[i].model.width;
+
+                divImage.appendChild(tagImage);
+
+                divContent.appendChild(divImage);
+                break;
+            case "list":
+                switch (jsonArticle.body[i].model.type) {
+                    case "unordered":
+                        var tagList = document.createElement("ul");
+                        for (var j = 0; j < jsonArticle.body[i].model.items.length; j++){
+                            var tagItem = document.createElement("li");
+                            tagItem.innerHTML = jsonArticle.body[i].model.items[j];
+                            tagList.appendChild(tagItem);
+                        }
+                        console.log(tagList.innerHTML);
+                        console.log(tagList.outerHTML);
+                        divContent.appendChild(tagList);
+                        break;
+
+                    case "ordered":
+                        var tagList = document.createElement("ol");
+                        for (var j = 0; j < jsonArticle.body[i].model.items.length; j++){
+                            var tagItem = document.createElement("li");
+                            tagItem.innerHTML = jsonArticle.body[i].model.items[j];
+                            tagList.appendChild(tagItem);
+                        }
+                        console.log(tagList.innerHTML);
+                        console.log(tagList.outerHTML);
+                        divContent.appendChild(tagList);
+                        break;
+                }
+
+                divContent.appendChild(tagList);
+                break;
+
+        }
+    }
+
+    generateNextButton(filename);
+
+    return true;
+}
+
+
+
+
+function ArticleExists(filename) {
+    for (var i = 0; i < arrArticles.length -1; i++){
+        if (filename == arrArticles[i].filename){
+            if (arrArticles[i].hasOwnProperty("json")){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 function generateNextButton(filename){
@@ -155,8 +206,9 @@ function generateNextButton(filename){
                 btnNext.type = "button";
                 btnNext.value = "Next";
                 btnNext.filename = arrArticles[i+1].filename;
+                getArticle(btnNext.filename, false);
                 btnNext.onclick = function(){
-                  getArticle(this.filename);  
+                  showArticle(this.filename);
                 };
                 divContent.appendChild(btnNext);
             }
@@ -170,9 +222,8 @@ function getRanking() {
     divContent.innerHTML = "";
 
     var divRankHelp = document.createElement("div");
-    divRankHelp.innerHTML = "<h3>Ranking</h3><p>You can give your rankings by using the up and down arrows to move the " +
-        "read articles. The higher the rank the better you thought the article was </p><p>After you have given" +
-        " your ranking you can submit them.</p>";
+    divRankHelp.innerHTML = "<h3>Ranking</h3><p>Please put the articles into the order you liked them, " +
+        "with the best at the top.</p><p>Please use the submit button to send your rankings to us.</p>";
     divContent.appendChild(divRankHelp);
 
     var tblRanking = document.createElement("table");
